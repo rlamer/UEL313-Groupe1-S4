131dbe61aeb2dd8da64616841251d1e927dc0160
Joana <111362154+qwjay78@users.noreply.github.com>
2024-12-22 20:11:02 +0100

    Update BlogController.php

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -1,19 +1,23 @@
 <?php
-
 namespace App\Controller;
+
 
 use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BlogController extends AbstractController
 {
-    #[Route('/blog', name: 'app_blog', methods: ['GET'])]
+    #[Route('/', name: 'app_blog')]
     public function index(ArticleRepository $articleRepository): Response
     {
+        // Récupère tous les articles
+        $articles = $articleRepository->findAll();
+
+        // Passe les articles au template
         return $this->render('blog/index.html.twig', [
-            'articles' => $articleRepository->findAll(),
+            'articles' => $articles,
         ]);
     }
 }
